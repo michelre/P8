4d35e010269409252700d71c3dcf5247fb5eeb07
remimichel <remi.michel38@gmail.com>
2018-04-07 11:52:06 +0200

    refactoring(): code review 2018-04-07

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
+/*global shortid, timestamp, $ */
 shortid = require('shortid');
-timestamp=require('time-stamp');
-
-/*//jquery
-require("jsdom").env("", function(err, window) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    
-    $ = require("jquery")(window);
-});*/
+timestamp = require('time-stamp');
+$ = require('jquery');
